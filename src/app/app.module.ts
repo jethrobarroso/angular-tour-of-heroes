7f04f6e2b99b03f40977d5d6c4168ed884db8a3a
Jethro <jethrobar@gmail.com>
2021-06-21 15:14:45 +0200

    feat: Create feature component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
@@ -18,4 +20,4 @@ import { FormsModule } from '@angular/forms';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {  }
