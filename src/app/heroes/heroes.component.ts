1cc712f4438583cc2d6bc59ea09d4ff2be531854
Jethro <jethrobar@gmail.com>
2021-06-24 09:58:10 +0200

    feat: Simulate backend data fetching

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,42 @@
 import { MessageService } from './../message.service';
 import { HeroService } from '../hero.service';
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  constructor(private heroSerice: HeroService, private messageService: MessageService) { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   heroes: Hero[] = [];
 
   getHeroes(): void {
-    this.heroSerice.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 
   ngOnInit(): void {
     this.getHeroes();
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+      .deleteHero(hero.id)
+      .subscribe(_ => this.heroes = this.heroes.filter((h) => h !== hero));
   }
 }
